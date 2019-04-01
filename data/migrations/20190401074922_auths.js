ad00d05c42e8552e90814f9171a94800da80ff99
korynewton <korynewtondev@gmail.com>
2019-04-01 08:43:42 -0700

    fixed user table

--- data/migrations/20190401074922_auths.js
+++ data/migrations/20190401074922_auths.js
@@ -2,7 +2,7 @@
 exports.up = function(knex) {
   return knex.schema.createTable('users', function(tbl){
       tbl.increments()
-      tbl.string('user', 100).notNullable().unique();
+      tbl.string('username', 100).notNullable().unique();
       tbl.string('password', 255).notNullable();
   })
 };
--- server.js
+++ server.js
@@ -1,11 +1,43 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const Users= require('./users/users-model')
 
 const server = express();
 
+server.use(helmet());
 server.use(express.json());
+server.use(cors())
+
+server.get('/', (req, res) => {
+    res.status(200).json({ message: "great success" })
+})
+
+server.post('/api/register', async (req, res) => {
+    const user = req.body;
+    // const hash = bcrypt.hashSync(user.password, 10)
+    
+    // user.password = hash;
+
+    const added = await Users.add(user);
+    try {
+        res.status(200).json(added)
+    } catch {
+        res.status(400).json( { message: "error" })
+    }
+
+})
 
 
 
+// server.post('/api/login', (req, res) => {
+
+// })
+// server.get('/api/users', (req, res) => {
+
+// })
 
 
 module.exports = server;
